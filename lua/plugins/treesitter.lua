21ab5162c8133902152053084614517f4b3c9b64
Noud <noudbonke@gmail.com>
2024-02-18 15:21:36 +0100

    initial commit

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+    "nvim-treesitter/nvim-treesitter", 
+    build = ":TSUpdate",
+    config = function()
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+            ensure_installed = {"c", "cpp", "cuda", "haskell", "lua", "python", "scala"},
+            highlight = {enable = true},
+            indent = {enable = true}
+        })
+    end
+}
+
