ac3357229c3036385b01ae49139d24efea3f184f
Noud <noudbonke@gmail.com>
2024-02-18 15:33:22 +0100

    added remaps and settings

--- lua/vim-settings.lua
+++ lua/vim-settings.lua
@@ -1,4 +1,4 @@
---tab expansion settings
+--Tab expansion settings
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
@@ -6,3 +6,24 @@ vim.cmd("set shiftwidth=4")
 
 vim.g.mapleader = " "
 
+-- Remap jk and kj to Esc in insert mode
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', 'kj', '<Esc>', {noremap = true, silent = true})
+
+-- Enable syntax highlighting
+vim.o.syntax = true
+
+-- Show line numbers
+vim.wo.number = true
+
+-- Disable swapfile
+vim.o.swapfile = false
+
+-- Highlight all search results
+vim.o.hlsearch = true
+
+-- Ignore case in search
+vim.o.ignorecase = true
+
+-- Show search results as you type
+vim.o.incsearch = true
